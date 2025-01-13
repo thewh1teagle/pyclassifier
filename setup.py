22f2df9d5aedc518166961523b25bf70a46c8361
neurlang <77860779+neurlang@users.noreply.github.com>
2025-01-13 19:23:13 +0100

    Bump the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hashtron",
-    version="0.0.8",
+    version="0.0.9",
     packages=find_packages(),
     install_requires=[
         "requests==2.32.3",
